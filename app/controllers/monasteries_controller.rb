4ce8ab35f4a3d6718b1c14122a7e3519fc87a739
wkdewey <william.dewey@unl.edu>
2023-02-10 16:41:50 -0600

    display facets alongside section search pages

--- app/controllers/monasteries_controller.rb
+++ app/controllers/monasteries_controller.rb
@@ -5,10 +5,15 @@ class MonasteriesController < ItemsController
 
     # query to return only cases
     options = params.permit!.deep_dup
-    options["f"] = ["category|Monasteries"]
+    if options["f"]
+      options["f"] << "category|Monasteries"
+    else
+      options["f"] = ["category|Monasteries"]
+    end
     @res = @items_api.query(options)
     # render search preset with route information
     @route_path = "home_path"
-    render_overridable "items", "search_preset"
+    @facet_limit = @section.present? ? SECTIONS[@section]["api_options"]["facet_limit"] : PUBLIC["api_options"]["facet_limit"]
+    render_overridable "items", "search_preset", locals: { res: @res}
   end
 end
--- app/controllers/religious_figures_controller.rb
+++ app/controllers/religious_figures_controller.rb
@@ -5,10 +5,15 @@ class ReligiousFiguresController < ItemsController
 
     # query to return only cases
     options = params.permit!.deep_dup
-    options["f"] = ["category|Religious figures"]
+    if options ["f"]
+      options ["f"] << "category|Religious figures"
+    else
+      options["f"] = ["category|Religious figures"]
+    end
     @res = @items_api.query(options)
     # render search preset with route information
     @route_path = "home_path"
-    render_overridable "items", "search_preset"
+    @facet_limit = @section.present? ? SECTIONS[@section]["api_options"]["facet_limit"] : PUBLIC["api_options"]["facet_limit"]
+    render_overridable "items", "search_preset", locals: { res: @res}
   end
 end
